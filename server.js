82648cd02f53141d50424b290063ae022d647080
Mateus <mateus.pereira@sistemafiep.org.br>
2025-02-26 20:22:08 -0300

    inicie a API com Node

---
+++ server.js
@@ -0,0 +1,18 @@
+import http from "http";
+
+const PORT = 3000;
+
+const rotas = {
+    "/": "API com Express e Node.js",
+    "/posts": "Rota de postagens",
+    "/autores": "Rota de autores",
+}
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-type": "text-plain" });
+    res.end(rotas[req.url]);
+});
+
+server.listen(PORT, () => {
+    console.log("Servidor na escuta!");
+});
